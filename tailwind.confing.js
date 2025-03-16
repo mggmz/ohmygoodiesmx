1c2fc3f977832c4a6869f19c59d808d742d06e12
mggmz <tonykill3@gmail.com>
2025-03-15 20:11:40 -0600

    fix (tailwind.config.js): fixed comment type block form

--- tailwind.confing.js
+++ tailwind.confing.js
@@ -1,9 +1,13 @@
-@type {import('tailwindcss').TailwindConfig}
+// tailwind.config.js
+/**
+ * @type {import('tailwindcss').Config}
+ */
 
 module.exports = {
-    content:[
-        ".pages/**/*.{js,jsx,ts,tsx}",
-        ".components/**/*.{js,jsx,ts,tsx,mdx}",
-        ".app/**/*.{js,jsx,ts,tsx,mdx}",
+    content: [
+        "./pages/**/*.{js,jsx,ts,tsx}",
+        "./components/**/*.{js,jsx,ts,tsx,mdx}",
+        "./app/**/*.{js,jsx,ts,tsx,mdx}",
     ],
-    
+    // ...otras configuraciones...
+};
